b74893306e379dcda63038e227c4ed5ccfe94871
Usama Blavins <usama.blavins1@gmail.com>
2023-09-12 23:13:33 +0100

    Create Event Bus rule for logging events to CloudWatch

--- events/test-event-bus/primary/main.tf
+++ events/test-event-bus/primary/main.tf
@@ -27,14 +27,37 @@ resource "aws_cloudwatch_event_bus" "podcast" {
   }
 }
 
-#resource "aws_cloudwatch_event_rule" "podcast" {
-#  description = "Log Events to CloudWatch"
-#  name        = "test-message-bus-event-logger"
-#
-#  event_pattern = jsonencode({
-#    account = ["903771974408"]
-#  })
-#}
+resource "aws_cloudwatch_event_rule" "podcast" {
+  description    = "Log Events to CloudWatch"
+  event_bus_name = aws_cloudwatch_event_bus.podcast.arn
+  is_enabled     = true
+  name           = "test-message-bus-event-logger-rule"
+  event_pattern  = jsonencode({
+    account = ["903771974408"]
+  })
+
+  tags = {
+    Name        = "test-message-bus-event-logger-rule"
+    Description = "Log Events to CloudWatch"
+  }
+}
+
+resource "aws_cloudwatch_event_target" "podcast_logging" {
+  arn            = aws_cloudwatch_log_group.podcast_log_group.arn
+  rule           = aws_cloudwatch_event_rule.podcast.name
+  event_bus_name = aws_cloudwatch_event_bus.podcast.arn
+  target_id      = "TestBusEventsLogGroup"
+}
+
+resource "aws_cloudwatch_log_group" "podcast_log_group" {
+  name              = "/aws/events/podcast/test"
+  retention_in_days = 30
+
+  tags = {
+    Name        = "/aws/events/podcast/test"
+    Description = "Cloud Watch Log Group for test-bus Events"
+  }
+}
 
 resource "aws_cloudwatch_event_archive" "podcast" {
   description      = "Event Archive for the test-message-bus"
